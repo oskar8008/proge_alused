3476d5d8410eff2b866be08cb0ad02d82354a5a9
oskar8008 <oskar-kaarel.kaljund@khk.ee>
2021-10-05 13:38:07 +0300

    1.2 president

---
+++ praks1/1.2 president.py
@@ -0,0 +1,8 @@
+#def muutujad ja omistan väärtused
+aasta = 2016
+president = "Kersti Kaljulaid"
+lause = ". aastal valiti Eesti president"
+
+#muutuhate kasutamine
+Lause = str(aasta) + lause + president
+print(lause)
