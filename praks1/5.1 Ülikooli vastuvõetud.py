20e86527c39bd2bd2934f178e9204da7825d580e
oskar8008 <oskar-kaarel.kaljund@khk.ee>
2021-11-16 15:03:26 +0200

    5.1 rebased.txt ja py fail

---
+++ praks1/5.1 Ülikooli vastuvõetud.py
@@ -0,0 +1,13 @@
+fail = open("rebased.txt", encoding="UTF-8")
+aasta = int(input("Mis aasta andmeid vajate: "))
+aasta1 = 2011
+if aasta >= 2020:
+    print(aasta, "aasta kohta ei ole veel andmeid")
+    
+for rida in fail:
+    if aasta == aasta1:
+        print(aasta1, " aastal oli vastuvõetuid ", rida)
+        break
+    aasta1 += 1
+    
+fail.close()
