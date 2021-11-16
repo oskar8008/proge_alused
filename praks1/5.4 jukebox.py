6275384a58b26f6f6c2736264af4be3af787ede2
oskar8008 <oskar-kaarel.kaljund@khk.ee>
2021-11-16 16:01:37 +0200

    5.4 jukebox ja txt failid

---
+++ praks1/5.4 jukebox.py
@@ -0,0 +1,13 @@
+fail = open(input("Sisestage playlisti nimi: jukebox.txt / 88ndad.txt / eesti_muusika.txt / edm.txt  "), encoding="UTF-8")
+laul = 1
+print("laulude valik: ")
+loend = []
+for rida in fail:
+    print(str(laul) + " " + str(rida[:-1]))
+    laul += 1
+    loend. append(rida)
+lauluvalimine = int(input("valige laulu number: "))
+lauluvalimine -= 1
+print("pr2egu m2ngib " + loend[lauluvalimine])
+fail.close()
+
