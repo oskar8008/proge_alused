5b6b63cff740ff08de4d872e67d310db3880004f
oskar8008 <oskar-kaarel.kaljund@khk.ee>
2021-11-23 14:35:11 +0200

    5.6 tahvel

---
+++ praks1/5.6 tahvli ette.py
@@ -0,0 +1,7 @@
+from datetime import *
+fail = open (input("Sisestage failinimi: (nimekiri.txt) "), encoding="UTF-8")
+nimekiri = []
+for nimi in fail:
+    nimekiri.append(nimi)
+print("Vastama tuleb: " + str(nimekiri[int(datetime.now().day) - 1]))
+fail.close
